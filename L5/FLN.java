afb166972027ca254a7c0af5c6ef215b6c6e86e0
Colin Timofei <ronin2120@gmail.com>
2021-06-18 01:25:27 +0300

    modified program with first and last name

--- L5/FLN.java
+++ L5/FLN.java
@@ -1,3 +1,7 @@
+import java.util.*;
+import java.io.*;
+import java.util.Random;
+
 public class FLN{
 	
 	// create object with 2 string variables
@@ -30,15 +34,36 @@ public class FLN{
 	public String toString()
 	{
 		return "prenume : " + this.firstName + "\n" +
-				"nume : " + this.lastName + "\n";
+				"nume : " + this.lastName;
 	}
 	
 	
-	// create a FirstLastName object, print it
+	// unknown object array size, use arraylist
+	// read from file, insert in array
+	// print one random name
 	
 	public static void main(String[] args)
 	{
-		FLN person1 = new FLN("Sanda", "Popescu");
-		System.out.print(person1);
+		ArrayList<FLN> fln = new ArrayList<FLN>();
+		int rn, size = 0;
+		Random rand = new Random();
+		
+		try ( BufferedReader br = new BufferedReader(new FileReader("a.txt")) ) {
+			String st, fn, ln;
+			int cat;
+			
+			while ((st = br.readLine()) != null) {
+				cat = st.indexOf(",");
+				if(cat != -1){
+					ln = st.substring(0, cat);
+					fn = st.substring(cat + 2, st.length());
+					fln.add(new FLN(fn, ln));
+					size++;
+				}
+			}
+		} catch(IOException e) { System.out.println("I/O Error: " + e); }
+		
+		rn = rand.nextInt(size);
+		System.out.println(fln.get(rn));
 	}
 }
